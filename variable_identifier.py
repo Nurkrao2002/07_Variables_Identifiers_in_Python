90ca03f042bde9ca6ea52b49446d737a047e4da0
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+uadaynalabothu@users.noreply.github.com>
2022-09-07 16:56:17 +0530

    Add files via upload

---
+++ variable_identifier.py
@@ -0,0 +1,45 @@
+""" Topic: variables using isidentifier  """
+
+print('a b'.isidentifier())
+print('a_b'.isidentifier())
+print('_'.isidentifier())
+print('x_y_c'.isidentifier())
+
+''' ways to assign variables  '''
+
+a=1 
+b=2
+print(a)
+print(b)
+
+a=1 
+b=2
+print(a);print(b)
+
+a=1 
+b=2
+print(a,b)
+
+a=1 
+b=2
+print(a,a)
+
+print(a);print(b) # NameError: name 'a' is not defined
+
+a=1 ; b=3
+print(a,b)
+print(a);print(b)
+
+x,y,z=4,5,6
+print(x,y,z)
+print(z,y,z)
+print(z)
+
+a=b=c=5
+print(a,b,c)
+
+a=d=4; b=6
+print(b,a)
+
+
+abcssasdasfsfasfagargavavsdvsd=6
